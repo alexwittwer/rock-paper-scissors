aa5b247926382deaf9952d4b4c50ca985bc9ab60
alexwittwer <wittweral@gmail.com>
2023-10-17 16:00:03 -0400

    add feature to complete game after 5 wins

--- script.js
+++ script.js
@@ -1,3 +1,83 @@
+//event listeners for buttons. adds all playButtons to btn nodelist
+//then iterates an event listener using each button's stored value
+const btn = document.querySelectorAll(".playButton");
+const usrScore = document.querySelector("#userScore");
+const cmpScore = document.querySelector("#computerScore");
+
+//creates a reset button that will be added and removed after win conditions
+
+/* function createResetButton() {
+  const resetButton = document.createElement("button");
+  const scoreboard = document.querySelector(".scoreboard");
+  resetButton.classList.add("resetButton");
+  scoreboard.insertBefore(resetButton, usrScore);
+  resetButton.textContent = "Play again?";
+}
+ */
+let userScore = 0;
+let compScore = 0;
+
+btn.forEach((item) => {
+  item.addEventListener("click", () => {
+    playRound(item.value);
+    winCon();
+    usrScore.textContent = "Player Score: " + userScore;
+    cmpScore.textContent = "Computer Score: " + compScore;
+  });
+});
+
+//initialize gameResult pointer
+const gameResult = document.querySelector(".gameResult");
+//clear gameResult text
+//current bug: cannot leave an empty string due to padding
+gameResult.textContent = "choose your weapon";
+
+//main game logic
+function playRound(playerChoice) {
+  let computerChoice = getComputerChoice();
+  let gameWon = winCon();
+
+  if (gameWon == true) {
+    return;
+  } else {
+    if (playerChoice === computerChoice) {
+      gameResult.textContent =
+        "It's a tie! Computer chose " + computerChoice + ". ";
+      return;
+    }
+    //win conditions
+    else if (
+      (playerChoice === "rock" && computerChoice === "scissors") ||
+      (playerChoice === "scissors" && computerChoice === "paper") ||
+      (playerChoice === "paper" && computerChoice === "rock")
+    ) {
+      gameResult.textContent =
+        "You win! Computer chose " + computerChoice + ".";
+      userScore += 1;
+      //lose conditions
+      return "user";
+    } else {
+      gameResult.textContent =
+        "You lost :( Computer chose " + computerChoice + ".";
+      compScore += 1;
+      return "computer";
+    }
+  }
+}
+
+//checks win conditions
+function winCon() {
+  if (userScore == 5) {
+    gameResult.textContent = "Victory!";
+    return true;
+  } else if (compScore == 5) {
+    gameResult.textContent = "Defeat...";
+    return true;
+  } else {
+    return false;
+  }
+}
+
 //get computer choice via math.random
 function getComputerChoice() {
   const computerChoice = Math.floor(Math.random() * 3) + 1;
@@ -13,101 +93,3 @@ function getComputerChoice() {
   }
   return outChoice;
 }
-
-//game logic -- button
-
-//event listeners for buttons. adds all playButtons to btn nodelist
-//then iterates an event listener using each button's stored value
-const btn = document.querySelectorAll(".playButton");
-const usrScore = document.querySelector("#userScore");
-const cmpScore = document.querySelector("#computerScore");
-let userScore = 0;
-let compScore = 0;
-  
-function playGame () {
-btn.forEach((item) => {
-  item.addEventListener("click", () => {
-    playRound(item.value);
-    usrScore.textContent = "Player Score: " + userScore;
-    cmpScore.textContent = "Computer Score: " + compScore;
-
-  });
-});
-}
-
-playGame();
-
-/*
-if (userScore < 5 || compScore < 5) {
-  playGame();
-} else if userScore === 5 {
-  gameResult.textContent = "Victory!";
-
-}else if compScore === 5 {
-  gameResult.textContent = "Defeat...";
-}
-*/
-
-//initialize gameResult pointer
-const gameResult = document.querySelector(".gameResult");
-//clear gameResult text
-//current bug: cannot leave an empty string due to padding
-gameResult.textContent = "choose your weapon";
-
-//main game logic
-function playRound(playerChoice) {
-  let computerChoice = getComputerChoice();
-
-  if (playerChoice === computerChoice) {
-    gameResult.textContent =
-      "It's a tie! Computer chose " + computerChoice + ". ";
-    return;
-  }
-  //win conditions
-  else if (
-    (playerChoice === "rock" && computerChoice === "scissors") ||
-    (playerChoice === "scissors" && computerChoice === "paper") ||
-    (playerChoice === "paper" && computerChoice === "rock")
-  ) {
-    gameResult.textContent = "You win! Computer chose " + computerChoice + ".";
-    userScore += 1;
-    //lose conditions
-    return "user";
-  } else {
-    gameResult.textContent =
-      "You lost :( Computer chose " + computerChoice + ".";
-      compScore += 1;
-    return "computer";
-  }
-}
-
-
-/* 
-//full game
-function playRPS() {
-    //initialize values for win condition
-    let userWinStreak = 0;
-    let computerWinStreak = 0;
-
-    //if user or computer has less than 5 wins, continue
-    while (userWinStreak < 5 && computerWinStreak < 5) {
-
-        //  play a round, save winner as output
-        const winner = playRound();
-
-        // handle win streak updates
-        if (winner === 'computer') {
-            computerWinStreak++;
-        } else if (winner === 'user') {
-            userWinStreak++;
-        }
-        // end the game, declare the winner
-        if (userWinStreak == 5) {
-            alert("You win!");
-            return;
-        } else if (computerWinStreak == 5) {
-            alert("You lost!");
-            return;
-        }
-    }
-} */
